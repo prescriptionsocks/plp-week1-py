0c711b8c18b1cd7ec59d66843fb6ee7b49dc724a
Baraka <barakaarunga@gmail.com>
2024-02-21 22:43:14 +0300

    This script takes user info and prints it out

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input("What's your name? ")
+age = str(input("What's your age? "))
+location = input("where do you live? ")
+print("Hey " + name + " , you are " + age + " years old and live in " + location)
+
